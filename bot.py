f5f8bac6292a35095650cb91363c40343cd1bf52
DIOR <diertojiev@yandex.ru>
2024-01-01 14:08:15 +0300

    Добавлена кнопка назад, пока что работает она не корректно

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import random
 from telebot import types
 from base_watches import base_watches
 
@@ -10,6 +11,29 @@ HELP_COMMAND = """
 /start - начать работу с ботом
 /about - о нас
 """
+
+def btn_company(message, select_wat):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn0 = types.KeyboardButton('MystaStyle')
+    btn1 = types.KeyboardButton('Rolex')
+    btn2 = types.KeyboardButton('Omega')
+    btn3 = types.KeyboardButton('Patek Philippe')
+    btn4 = types.KeyboardButton('Audemars Piguet')
+    btn5 = types.KeyboardButton('Tag Heuer')
+    btn6 = types.KeyboardButton('Seiko')
+    btn7 = types.KeyboardButton('Casio')
+    btn8 = types.KeyboardButton('Fossil')
+    btn9 = types.KeyboardButton('Citizen')
+    btn10 = types.KeyboardButton('Timex')
+    btn11 = types.KeyboardButton('Swatch')
+    btn12 = types.KeyboardButton('Tissot')
+    btn13 = types.KeyboardButton('Bulova')
+    btn14 = types.KeyboardButton('Michael Kors')
+    btn15 = types.KeyboardButton('DIOR')
+
+    markup.add(btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15)
+    bot.send_message(message.chat.id, select_wat, parse_mode='html', reply_markup=markup)
+
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
@@ -29,41 +53,20 @@ def start_handler(message):
 def about_handler(message):
     bot.send_message(message.chat.id, f'MustaStyle — ваш надежный проводник в мире элегантности и точности времени. Наша компания, основанная студентом-программистом Мустафой, сочетает в себе страсть к инновациям, высокому стилю и уважению к времени.\n\nМы специализируемся на продаже часов, которые не только служат функциональным измерителем времени, но и становятся неотъемлемой частью вашего стиля. В ассортименте MustaStyle вы найдете коллекции, вдохновленные последними трендами моды, классическим дизайном и инновационными технологиями.\n\nНаша цель - не просто предоставлять товар, а создавать уникальный опыт для наших клиентов. Мы стремимся удовлетворить запросы даже самых требовательных ценителей часов. Каждая модель внимательно отобрана и прошла тщательный контроль качества, чтобы удовлетворить ожидания даже самых искушенных покупателей.\n\nMustaStyle гордится своей преданностью качеству и предоставлению беспрецедентного уровня обслуживания. Наши сотрудники обладают глубокими знаниями в области часового искусства и всегда готовы поделиться своими знаниями, чтобы помочь вам сделать правильный выбор.\n\nПокупка часов в MustaStyle - это не просто приобретение аксессуара, но и инвестиция в свой стиль и уникальность. Доверьтесь нам, и мы сделаем ваш выбор часов незабываемым и приятным моментом.\n\nВыберите MustaStyle — выберите стиль, выберите качество, выберите надежность. Мы заботимся о вашем времени, чтобы вы могли наслаждаться каждым моментом в стиле MustaStyle.')
 
-
 @bot.message_handler(func=lambda message: message.text == 'Русский 🇷🇺')
 def select_watch(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     select_watch = "Теперь выберите фирму часов, которую бы вы хотели приобрести."
-    btn0 = types.KeyboardButton('MystaStyle')
-    btn1 = types.KeyboardButton('Rolex')
-    btn2 = types.KeyboardButton('Omega')
-    btn3 = types.KeyboardButton('Patek Philippe')
-    btn4 = types.KeyboardButton('Audemars Piguet')
-    btn5 = types.KeyboardButton('Tag Heuer')
-    btn6 = types.KeyboardButton('Seiko')
-    btn7 = types.KeyboardButton('Casio')
-    btn8 = types.KeyboardButton('Fossil')
-    btn9 = types.KeyboardButton('Citizen')
-    btn10 = types.KeyboardButton('Timex')
-    btn11 = types.KeyboardButton('Swatch')
-    btn12 = types.KeyboardButton('Tissot')
-    btn13 = types.KeyboardButton('Bulova')
-    btn14 = types.KeyboardButton('Michael Kors')
-    btn15 = types.KeyboardButton('DIOR')
-
-    markup.add(btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15)
-    bot.send_message(message.chat.id, select_watch, parse_mode='html', reply_markup=markup)
-
+    btn_company(message, select_watch)
 
     def create_company_handler(company, models):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         buttons = [types.KeyboardButton(model) for model in models]
-        markup.add(*buttons)
+        back_btn = types.KeyboardButton('← Назад')
+        markup.add(*buttons, back_btn)
 
         url_markup =  types.InlineKeyboardMarkup()
         url_btns = types.InlineKeyboardButton('ОФОРМИТЬ', url='https://mustafapulse.ru/')
         url_markup.add(url_btns)
-        
 
         @bot.message_handler(func=lambda message, comp=company: message.text == comp)
         def handle_company(message):
@@ -82,10 +85,23 @@ def select_watch(message):
                 price_range = mods[chosen_model]
                 bot.send_message(message.chat.id, f"Прекрасно! Ваш выбор пал на модель часов {chosen_model}, стоимостью в пределах {price_range} в зависимости от комплектации. Хотите оформить заказ сейчас?)", reply_markup=url_markup)
 
+        @bot.message_handler(func=lambda message: message.text.lower() == '← назад')
+        def back_to_company_selection(message):
+            companies = ['MystaStyle', 'Rolex', 'Omega', 'Patek Philippe', 'Audemars Piguet', 'Tag Heuer', 'Seiko',
+                    'Casio', 'Fossil', 'Citizen', 'Timex', 'Swatch', 'Tissot', 'Bulova', 'Michael Kors', 'DIOR']
+            random_company = random.choice(companies)
+            select_watch = f"Может рассмотрим часы от {random_company}?"
+            btn_company(message, select_watch)
+
+            for company, models in base_watches.items():
+                create_company_handler(company, models)
+
     for company, models in base_watches.items():
         create_company_handler(company, models)
 
-    @bot.message_handler(func=lambda message: message.text == btn0.text)
+
+
+    @bot.message_handler(func=lambda message: message.text.lower() == 'mystastyle')
     def models(message):
 
         url_markup =  types.InlineKeyboardMarkup()
@@ -101,38 +117,16 @@ def select_watch(message):
 
 
 
-
-
-
 @bot.message_handler(func=lambda message: message.text == 'English 🇬🇧')
 def select_watch(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     select_watch = "Now select the watch company that you would like to purchase."
-    btn0 = types.KeyboardButton('MystaStyle')
-    btn1 = types.KeyboardButton('Rolex')
-    btn2 = types.KeyboardButton('Omega')
-    btn3 = types.KeyboardButton('Patek Philippe')
-    btn4 = types.KeyboardButton('Audemars Piguet')
-    btn5 = types.KeyboardButton('Tag Heuer')
-    btn6 = types.KeyboardButton('Seiko')
-    btn7 = types.KeyboardButton('Casio')
-    btn8 = types.KeyboardButton('Fossil')
-    btn9 = types.KeyboardButton('Citizen')
-    btn10 = types.KeyboardButton('Timex')
-    btn11 = types.KeyboardButton('Swatch')
-    btn12 = types.KeyboardButton('Tissot')
-    btn13 = types.KeyboardButton('Bulova')
-    btn14 = types.KeyboardButton('Michael Kors')
-    btn15 = types.KeyboardButton('DIOR')
-
-    markup.add(btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14, btn15)
-    bot.send_message(message.chat.id, select_watch, parse_mode='html', reply_markup=markup)
-
+    btn_company(message, select_watch)
 
     def create_company_handler(company, models):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         buttons = [types.KeyboardButton(model) for model in models]
-        markup.add(*buttons)
+        back_btn = types.KeyboardButton('← Back')
+        markup.add(*buttons, back_btn)
 
         url_markup =  types.InlineKeyboardMarkup()
         url_btns = types.InlineKeyboardButton('PLACE AN ORDER', url='https://mustafapulse.ru/')
@@ -156,10 +150,22 @@ def select_watch(message):
                 price_range = mods[chosen_model]
                 bot.send_message(message.chat.id, f"Perfectly! Your choice fell on the watch model {chosen_model}, cost within the limits of {price_range} depending on the configuration. Do you want to place an order now?)", reply_markup=url_markup)
 
+        @bot.message_handler(func=lambda message: message.text.lower() == '← back')
+        def back_to_company_selection(message):
+            companies = ['MystaStyle', 'Rolex', 'Omega', 'Patek Philippe', 'Audemars Piguet', 'Tag Heuer', 'Seiko',
+                    'Casio', 'Fossil', 'Citizen', 'Timex', 'Swatch', 'Tissot', 'Bulova', 'Michael Kors', 'DIOR']
+            random_company = random.choice(companies)
+            select_watch = f"Maybe consider a watch from {random_company}?"
+            btn_company(message, select_watch)
+
+            for company, models in base_watches.items():
+                create_company_handler(company, models)
+
+
     for company, models in base_watches.items():
         create_company_handler(company, models)
 
-    @bot.message_handler(func=lambda message: message.text == btn0.text)
+    @bot.message_handler(func=lambda message: message.text.lower() == 'mystastyle')
     def models(message):
 
         url_markup =  types.InlineKeyboardMarkup()
@@ -167,6 +173,7 @@ def select_watch(message):
         url_markup.add(url_btns)
 
         bot.send_message(message.chat.id, 'Wow, MustaStyle is your choice! We will be happy to offer you a unique watch that suits your taste, color scheme and budget. Just tell us which watch you prefer by leaving a request on our website, and we will contact you as soon as possible. Trust us to create your perfect time accessory!', reply_markup=url_markup)
+
 
 
 bot.polling(none_stop=True)
